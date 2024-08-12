d56483777740a2fbc7408fc834503470826a4bbb
Tiago Garbi <tgbmateriais@gmail.com>
2024-08-12 18:14:15 -0300

    feature: include logic to update completed task

--- src/controllers/tasks/updateComplete.task.js
+++ src/controllers/tasks/updateComplete.task.js
@@ -1,17 +1,17 @@
 import { database } from "../../database/index.js";
 
 export const updateComplete = (req, res) => {
-    const { id } = req.params
+    const { id } = req.params;
 
-    const task = database.select('tasks', { id });
+    const task = database.select("tasks", { id });
 
     const isTaskCompleted = Boolean(task.completed_at);
 
-    if(!isTaskCompleted) return res.writeHead(404).end();
+    if (isTaskCompleted) return res.writeHead(404).end();
 
-    const completed_at =  new Date();
+    const completed_at = new Date();
 
-    database.update('tasks', id, { completed_at })
+    database.update("tasks", id, { completed_at });
 
-    return res.writeHead(204).end()
-}
+    return res.writeHead(204).end();
+};
